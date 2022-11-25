dc4783b7e1ca0e428aae7b3310f4abb6e5757af5
mehmetali10 <mhmtali2316@gmail.com>
2022-11-26 01:31:49 +0300

    add cities

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import turtle
-import images
+import city
 
 found = []
 
@@ -25,18 +25,14 @@ def isValid(list, key, value):
 
 
 
-cities = [{"id":22,"city":"Edirne", "x": -515.0, "y": 197.0 ,"rotate": 0, "fontsize":12 },
-{"id":39,"city":"Kırklareli", "x": -459.0, "y": 238.0 ,"rotate": 0, "fontsize":12 },
-{"id":59,"city":"Tekirdağ", "x": -468.0, "y": 187.0 ,"rotate": 0 ,"fontsize":12 },
-{"id":34,"city":"İstanbul", "x": -402.0, "y": 191.0 ,"rotate": 0, "fontsize":12 },
-{"id":41,"city":"Kocaeli", "x": -319.0 , "y":159.0 ,"rotate": 0, "fontsize":12 },
-{"id":77,"city":"Yalova", "x":-371.0, "y":145.0 ,"rotate": 0, "fontsize":12 }]
+cities = city.getCities()
 
 image = "./images/unnamed.gif"
 screen = turtle.Screen()
-screen.setup(1250,712)
+screen.setup(1250, 712)
 screen.addshape(image)
 input = ""
+
 
 while input != "-1":
     turtle.shape(image)
@@ -46,3 +42,4 @@ while input != "-1":
     result = isValid(cities,"city", input.strip())
     if result != {}:
         putText(result["city"], int(result["x"]), int(result["y"]))
+
